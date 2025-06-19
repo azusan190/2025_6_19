1af685b3f4d746082199e81e2c554b7446fdb5d0
azusan190 <ttc2550sg0005@edu.tech.ac.jp>
2025-06-19 11:00:12 +0900

    ファイルの追加
    キャンディを消す動作と飛ばす動作を追加した

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject candy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            CandyPop();
+        };
+    }
+    public void CandyPop() 
+    {
+        GameObject candyPrefab = Instantiate(candy,transform);
+
+        candyPrefab.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+    }
+}
